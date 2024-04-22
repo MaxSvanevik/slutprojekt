7854974994022f8b4fe4cdfec34bdd746bb90543
MaxSvanevik <maxsvanevik4@gmail.com>
2024-04-22 11:13:36 +0200

    började med type effectiveness och sorterade ut alla alternative forms

--- temp.py
+++ temp.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
-poke = pd.read_csv("Pokemon.csv")
-poke = poke.sort_values("Form", ascending=False)
+# poke = pd.read_csv("Pokemon.csv")
+# poke = poke.sort_values("Form", ascending=False)
 pd.set_option("display.max_rows", None)
-poke = poke.replace(" ", "None")
-print(poke.head(222))
+# poke = poke.replace(" ", "None")
+# poke = poke.drop_duplicates(subset="ID", keep="last")
+# poke = poke.sort_values("ID", ascending=True)
+# print(poke.head(1010))
+# poke.to_csv('Pokemon2.csv', index=False)
+poke = pd.read_csv("Pokemon2.csv")
+print(poke)
---
+++ type.py
@@ -0,0 +1,86 @@
+
+
+normal = {
+    "rock" : 0.5,
+    "ghost" : 0,
+    "steel" : 0.5
+}
+
+fire = {
+    "fire" : 0.5,
+    "water" : 0.5,
+    "grass" : 2,
+    "ice" : 2,
+    "bug" : 2,
+    "rock" : 0.5,
+    "dragon" : 0.5,
+    "steel" : 2
+}
+
+water = {
+    "fire" : 2,
+    "water" : 0.5,
+    "grass" : 0.5,
+    "ground" : 2,
+    "rock" : 2,
+    "dragon" : 0.5
+}
+
+grass = {
+    "fire" : 0.5,
+    "water" : 2,
+    "grass" : 0.5,
+    "poison" : 0.5,
+    "ground" : 2,
+    "flying" : 0.5,
+    "bug" : 0.5,
+    "rock" : 2,
+    "dragon" : 2,
+    "steel" : 2
+}
+
+electric = {
+    "water" : 2,
+    "grass" : 0.5,
+    "electric" : 0.5,
+    "ground" : 0,
+    "flying" : 2,
+    "dragon" : 0.5
+}
+
+ice = {
+    "fire" : 0.5,
+    "water" : 0.5,
+    "grass" : 2,
+    "ice" : 0.5,
+    "ground" : 2,
+    "flying" : 2,
+    "dragon" : 2,
+    "steel" : 2,
+}
+
+fighting = {
+    "normal" : 2,
+    "ice" : 2,
+    "poison" : 0.5,
+    "flying" : 0.5,
+    "psychic" : 0.5,
+    "bug" : 0.5,
+    "rock" : 2,
+    "ghost" : 0,
+    "dark" : 2,
+    "steel" : 2,
+    "fairy" : 0.5
+}
+
+poison = {}
+ground = {}
+flying = {}
+psychic = {}
+bug = {}
+rock = {}
+ghost = {}
+dragon = {}
+dark = {}
+steel = {}
+fairy = {}
