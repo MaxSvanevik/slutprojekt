1967c22916f1aa7e8054bff4fd19bfbc6bc09946
MaxSvanevik <maxsvanevik4@gmail.com>
2024-04-25 13:03:25 +0200

    jag kollade mest upp hur man gjorde saker

--- pokemonTypes.py
+++ pokemonTypes.py
@@ -1,11 +1,16 @@
 import pandas as pd
 
+pd.set_option("display.max_rows", None)
+poke = pd.read_csv("Pokemon2.csv")
+types = ["Water", "Flying", "Normal", "Dragon", "Fairy"]
 
-def types(type1 = input("ange en pokemon type"), type2 = input("ange en till (lämna blank för ingen secondary type)")):
-    poke = pd.read_csv("Pokemon2.csv")
-    poke = poke[poke["Type1"] == type1]
-    poke = poke[poke["Type2"] == type2]
-    print(poke)
 
-types()
-    
+# poke = poke[(poke["Type1"] == types[0]) | (poke["Type1"] == types[1])]
+poke = poke[(poke["Type1"] == types[0]) | (poke["Type2"] == types[0])]
+# poke = poke[(poke["Type1"] == types[1]) | (poke["Type2"] == types[1])]
+
+poke = poke[(poke["Attack"] > 100)]
+poke = poke[(poke["Generation"] <= 4) | (poke["Generation"] == 8)]
+
+
+print(poke)
