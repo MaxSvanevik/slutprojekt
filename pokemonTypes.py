d7c696ecb5d422082318d8b0e746740c222d3b59
MaxSvanevik <maxsvanevik4@gmail.com>
2024-04-26 11:00:37 +0200

    jag gjorde typ klart filtreringen men den är inte riktigt användar vänlig än

--- pokemonTypes.py
+++ pokemonTypes.py
@@ -2,15 +2,42 @@ import pandas as pd
 
 pd.set_option("display.max_rows", None)
 poke = pd.read_csv("Pokemon2.csv")
-types = ["Water", "Flying", "Normal", "Dragon", "Fairy"]
+allTypes = [
+    "Normal", "Fire", "Water", "Electric", "Grass", "Ice", 
+    "Fighting", "Poison", "Ground", "Flying", "Bug", "Dark",
+    "Rock", "Ghost", "Dragon", "Psychic", "Steel", "Fairy"
+]
+types = []
+
+x = "."
+while x != "":
+    x = input("vilken typ vill du lägga till?\nskriv help för att få en lista av alla typer\n").lower().capitalize().strip()
+    if x == "Help":
+        print(allTypes)
+    else:
+        types.append(x)
 
 
-# poke = poke[(poke["Type1"] == types[0]) | (poke["Type1"] == types[1])]
-poke = poke[(poke["Type1"] == types[0]) | (poke["Type2"] == types[0])]
-# poke = poke[(poke["Type1"] == types[1]) | (poke["Type2"] == types[1])]
+poke = poke[poke["Type1"].isin(types)]
+poke = poke[poke["Type2"].isin(types)]
 
-poke = poke[(poke["Attack"] > 100)]
-poke = poke[(poke["Generation"] <= 4) | (poke["Generation"] == 8)]
+
+x = input("vill du sortera efter något annat?\n")
+y = int(input(""))
+z = input("större eller mindre?")
+
+if x in {"Total","HP","Attack","Defense","Sp. Atk","Sp. Def","Speed","Generation"}:
+
+    if z == "större":
+        poke = poke[poke[x] > y]
+
+    elif z == "mindre":
+        poke = poke[poke[x] < y]
+    
+    else:
+        poke = poke[poke[x] == y]
+    
+
 
 
 print(poke)
